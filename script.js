659225e1c307ecac8034feff0b7477d37f7c2ac4
Kikuchi-Kazuki <102280416+Kikuchi-Kazuki@users.noreply.github.com>
2022-05-11 21:36:05 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,64 +1,183 @@
-body {
-  font-family: "游ゴシック", "ヒラギノ角ゴシック", "メイリオ", sans-serif;
-  background-color: white;
-  color: black;
+'use strict'
+// 1行目に記載している 'use strict' は削除しないでください
+
+const soundsList = {
+  startSound: "sounds/start.mp3",
+  ponSound: "sounds/pon.mp3",
+  aikodeSound: "sounds/aiko.mp3",
+  shoSound: "sounds/sho.mp3",
+  loseSound: "sounds/zuko-.mp3",
+  WinSound: "sounds/fever_yappi.mp3"
+}
+//スタートボタンの動作
+// let count = 3; //残回数のカウント
+function clickStartButton() {
+  // document.getElementById("game_start").innerHTML = count; //残回数のカウントを表示する
+  document.getElementById("gu").disabled = false;
+  document.getElementById("choki").disabled = false;
+  document.getElementById("pa").disabled = false;
+  // document.getElementById("start_message").innerHTML = "じゃんけん";
+  document.getElementById('ImagOfJankenman').src = 'images/janken_animation_20msec.gif';
+  const music = new Audio(soundsList.startSound);
+  music.play();
+}
+//Rock-paper-scissors
+//Hand 0：グー 1：チョキ 2:パー
+function controlButton() {
+  document.getElementById("start").disabled = true;
+  document.getElementById('ImagOfJankenman').src = 'images/janken_animation_20msec.gif';
+  //じゃんけんボタンの動作を制御する
+  /*
+    count = count - 1;  //残回数のカウントを減らす
+    if (count >= 0) {
+      document.getElementById("game_start").innerHTML = count; //残回数のカウントを表示する
+      document.getElementById("start").disabled = true;
+      document.getElementById('ImagOfJankenman').src = 'images/janken_animation_20msec.gif';
+  
+    } else {
+      document.getElementById("game_start").innerHTML = "GAME OVER";
+      document.getElementById("gu").disabled = true;
+      document.getElementById("choki").disabled = true;
+      document.getElementById("pa").disabled = true;
+      count = 3;
+    }
+      */
 }
 
-h1 {
-  color: #f8a;
-  font-weight: bold;
-  text-shadow: black;
+function playPon() {
+  const music = new Audio(soundsList.ponSound);
+  music.play();
+}
+function playSho() {
+  const music = new Audio(soundsList.shoSound);
+  music.play();
 }
 
-h1,
-h2 {
-  text-align: center;
-}
-
-
-p.img_center {
-  text-align: center;
-}
-
-.inner {
-  padding-top: 30px;
-  display: flex;
-  justify-content: space-evenly;
-}
-
-.box {
-  color: #fff;
-  background-color: red;
-  border: 1px solid black;
-  line-height: 2.5;
-  padding: 0 20px;
-  text-shadow: 1px 1px 1px black;
-  border-radius: 10px;
-}
-
-.box:hover {
-  box-shadow:
-  inset -2px -2px 3px white,
-  inset 2px 2px 3px black;
-}
-
-.box:active {
-  background-color: darkred;
+function finishJanken() {
+  document.getElementById("start").disabled = false;
+  document.getElementById("gu").disabled = true;
+  document.getElementById("choki").disabled = true;
+  document.getElementById("pa").disabled = true;
+  document.getElementById('ImagOfJankenman').src = 'images/janken_animation_1000msec.gif';
+  jankenResult = "finish";
 }
 
 
+// function selectJankenmanHand() {
+//   //ジャンケンマンの手を抽選する
+//   const jankenmanHand = Math.trunc(Math.random() * 3);
+//   if (jankenmanHand === 0) {
+//     document.getElementById('ImagOfJankenman').src = 'images/gu_stone_black.jpg';
+//   } else if (jankenmanHand === 1) {
+//     document.getElementById('ImagOfJankenman').src = 'images/choki_scissors_black.jpg';
+//   } else {
+//     document.getElementById('ImagOfJankenman').src = 'images/pa_paper_black.jpg';
+//   }
+//   return jankenmanHand;
+// }
 
-/* 点滅 */
-.blink {
-  animation: blinking 1s ease-in-out infinite alternate;
-}
+function selectJankenmanHand(userHand) {
+  //ジャンケンマンの手を抽選する
+  // const jankenmanHand = Math.trunc(Math.random() * 3);
+  let jankenmanHand;
+  const randomNumber = Math.trunc(Math.random() * 5); //0,1,2,3,4
+  console.log(randomNumber)
+  if (userHand === 0) { //ユーザーがグーを選んだとき
+    if (randomNumber === 1) {
+      jankenmanHand = 1;
+    } else if (randomNumber === 2 || randomNumber === 3) {
+      jankenmanHand = 2;
+    } else {
+      jankenmanHand = 0;
+    }
 
-/* @keyframes blinking {
-  0% {
-    opacity: 0;
+  } else if (userHand === 1) { //ユーザーがチョキを選んだとき
+    if (randomNumber === 2) {
+      jankenmanHand = 2;
+    } else if (randomNumber === 0 || randomNumber === 3) {
+      jankenmanHand = 0;
+    } else {
+      jankenmanHand = 1;
+    }
+  } else { //ユーザーがパーを選んだとき
+    if (randomNumber === 0) {
+      jankenmanHand = 0;
+    } else if (randomNumber === 1 || randomNumber === 3) {
+      jankenmanHand = 1;
+    } else {
+      jankenmanHand = 2;
+    }
   }
-
-  100% {
-    opacity: 1;
+  console.log(jankenmanHand);
+  if (jankenmanHand === 0) {
+    document.getElementById('ImagOfJankenman').src = 'images/gu_stone_black.jpg';
+  } else if (jankenmanHand === 1) {
+    document.getElementById('ImagOfJankenman').src = 'images/choki_scissors_black.jpg';
+  } else {
+    document.getElementById('ImagOfJankenman').src = 'images/pa_paper_black.jpg';
   }
-} */
+  return jankenmanHand;
+}
+
+
+
+let jankenResult = 0;
+const gameCounter = {};
+function janken(userHand) {
+
+  if (jankenResult === "draw") {
+    playSho();
+  } else {
+    playPon();
+  }
+  const jankenmanHand = selectJankenmanHand(userHand);
+  console.log(userHand);
+console.log(jankenmanHand);
+  if (userHand === jankenmanHand) {
+    console.log("aiko");
+    setTimeout(() => {
+      const music = new Audio(soundsList.aikodeSound);
+      music.play();
+    }, 500); // Execution 0.5sec
+    setTimeout(() => {
+      document.getElementById('ImagOfJankenman').src = 'images/janken_animation_20msec.gif';
+    }, 700); // Execution 0.7sec
+    jankenResult = "draw";
+
+  } else if (userHand === 0 && jankenmanHand === 1) {
+    console.log("Win (selected Hand -> Gu)");
+    setTimeout(() => {
+      const music = new Audio(soundsList.WinSound);
+      music.play();
+    }, 500); // Execution 0.5sec
+    setTimeout(() => {
+      finishJanken();
+    }, 6000); // Execution 6sec
+    } else if (userHand === 1 && jankenmanHand === 2) {
+    console.log("Win (selected Hand -> Choki)")
+    setTimeout(() => {
+      const music = new Audio(soundsList.WinSound);
+      music.play();
+    }, 500); // Execution 0.5sec
+    setTimeout(() => {
+    finishJanken();
+  }, 6000); // Execution 6sec
+  } else if (userHand === 2 && jankenmanHand === 0) {
+    console.log("Win (selected Hand -> Pa)")
+    setTimeout(() => {
+      const music = new Audio(soundsList.WinSound);
+      music.play();
+    }, 500); // Execution 0.5sec
+    setTimeout(() => {
+      finishJanken();
+    }, 6000); // Execution 6sec
+
+  } else {
+    console.log("You lose!")
+    setTimeout(() => {
+      const music = new Audio(soundsList.loseSound);
+      music.play();
+    }, 500); // Execution 0.5sec
+    finishJanken();
+  }
+}
